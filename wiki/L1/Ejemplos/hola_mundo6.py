f655eded42cde5acb93131b73ca6e5cb4a7ebc09
obijuan <obijuan.cube@gmail.com>
2025-02-10 22:07:41 +0100

    L1: add example 6

---
+++ wiki/L1/Ejemplos/hola_mundo6.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────────
+# ── Hola mundo 6: Usando el paquete colorama
+# ──────────────────────────────────────────────────────
+# ── Imprimir mensajes "Hola mundo" en colores...
+# ──────────────────────────────────────────────────────
+import colorama
+
+print("══════════════════════════════")
+print("Hola mundo normal...")
+print(f"{colorama.Fore.RED}Hola mundo en rojo...")
+print(f"{colorama.Fore.GREEN}Hola mundo en verde...")
+#-- Reseteamos el color
+print(colorama.Style.RESET_ALL, end="")
+print("──────────────────────────────")
+
+
+
+

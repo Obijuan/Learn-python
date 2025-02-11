a7c8d0d992b91067fe03aa8a7cbc60e9ca44eb6d
obijuan <obijuan.cube@gmail.com>
2025-02-11 20:00:17 +0100

    L1: update black-test

--- wiki/L1/Ejemplos/Black-test.py
+++ wiki/L1/Ejemplos/Black-test.py
@@ -3,4 +3,6 @@
 # ──────────────────────────────────────────────────────
 import colorama
 print(        "══════════════════════════════"    )
-
+print(  f"{colorama.Fore.RED}Hola mundo en rojo..."    )
+for i   in   range(4,20,2)    :
+    print(    i    )

ffe72413e7771bf98595e25eebfa592b35e3cc02
obijuan <obijuan.cube@gmail.com>
2025-05-18 22:16:25 +0200

    L2: add example 6

---
+++ wiki/L2/Ejemplos/06-cadenas-multilinea.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# -- Todas estas impresiones en consola producen
+# -- el mismo resultado
+print("""LINEA 1
+LINEA 2
+LINEA 3
+""")
+
+print("""\
+LINEA 1
+LINEA 2
+LINEA 3
+""")
+
+print(
+    """\
+LINEA 1
+LINEA 2
+LINEA 3
+""")
+
+# -- Si no se pone \, se añade el \n tras """"
+print("""
+LINEA 1
+LINEA 2
+LINEA 3
+""")

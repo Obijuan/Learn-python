afbc4715437a3984a7c29baf005ad90b6ddecd2b
obijuan <obijuan.cube@gmail.com>
2025-05-18 21:43:24 +0200

    Add ejemplo 5

---
+++ wiki/L2/Ejemplos/05-cadenas-lit-concat.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# -- Una cadena formada por dos subcadenas concatenadas
+print()
+print("Hola " "Mundo")
+print("───────" "───────")
+print()
+
+# -- Multilineas con la opción 1
+print("LINEA 1")
+print("LINEA 2")
+print("LINEA 3")
+print()
+
+# -- Multilineas con la opción 2
+print("LINEA 1\nLINEA 2\nLINEA 3\n")
+
+# -- Multilíneas con cadenas literales concatenadas
+print("LINEA 1\n"
+      "LINEA 2\n"
+      "LINEA 3\n")

2dcb19930af0d304c00eac43df657f76d71eddc7
obijuan <obijuan.cube@gmail.com>
2025-05-18 23:01:37 +0200

    L2: add example 8

---
+++ wiki/L2/Ejemplos/08-len-car.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# -- Cadenas de pruebas
+cad1 = "1234"
+cad2 = "1234" "5678"
+cad3 = "😀😎😉"
+cad4 = "ñoño"
+cad5 = "áéíóú"
+cad6 = "Hola\n"
+
+# -- Obtener las longitudes
+len1 = len(cad1)
+len2 = len(cad2)
+len3 = len(cad3)
+len4 = len(cad4)
+len5 = len(cad5)
+len6 = len(cad6)
+
+# -- Mostrar resultados
+print()
+print(f"Cadena {cad1} --> Longitud: {len1}")
+print(f"Cadena {cad2} --> Longitud: {len2}")
+print(f"Cadena {cad3} --> Longitud: {len3}")
+print(f"Cadena {cad4} --> Longitud: {len4}")
+print(f"Cadena {cad5} --> Longitud: {len5}")
+print(f"Cadena {cad6} --> Longitud: {len6}")
+print()

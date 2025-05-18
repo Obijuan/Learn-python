43c317bb3811944de874877bfd1ac80915d299af
obijuan <obijuan.cube@gmail.com>
2025-05-18 22:42:09 +0200

    L2: add example 7

---
+++ wiki/L2/Ejemplos/07-cadenas-format.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# ----------------------------------
+# -- Ejemplo de cadena formateada
+# ----------------------------------
+
+# -- Definimos las variables
+v1 = 1
+v2 = 1
+suma = v1 + v2
+resta = v1 - v2
+
+# -- Cadena formateada
+cad = f"La suma de {v1} + {v2} es {suma}"
+
+# -- Imprimimos la cadena
+print(cad)
+
+# -- La cadena formateada se puede poner dentro de
+# -- la instruccion de print
+print(f"La resta de {v1} menos {v2} es {resta}")

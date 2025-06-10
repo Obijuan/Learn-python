71bb140a8b7e06dd323fc74167c499382bb70567
obijuan <obijuan.cube@gmail.com>
2025-06-10 13:08:48 +0200

    L2: add example 9

---
+++ wiki/L2/Ejemplos/09-operador-asterisco.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+print()
+# -- Imprimir la cadena "Hola" 3 veces
+print("Hola " * 3)
+
+# -- Subrayado de 40 caracteres
+print("─" * 40)
+
+cad = "El operador * mola mucho!!"
+
+# -- Imprimir un mensaje y subrayarlo
+print(f"{cad}")
+print('─' * len(cad))

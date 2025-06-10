727dd1d3bf6e03274e2dc751ca40f1d0bd6d21f2
obijuan <obijuan.cube@gmail.com>
2025-06-10 13:46:07 +0200

    L2: add image

---
+++ wiki/L2/Ejemplos/10-print-sep.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+cad1 = "Cadena1"
+cad2 = "Cadena2"
+num = 5
+
+# -- Imprimir diferentes objetos usando el separador
+# -- por defecto (un espacio)
+print("Probando", cad1, num, cad2)
+
+# -- Imprimir los mismos objetos pero usando como
+# -- separador una coma (,)
+print("Probando", cad1, num, cad2, sep=",")

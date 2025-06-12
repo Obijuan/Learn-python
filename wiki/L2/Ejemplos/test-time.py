dc632fc815ebbc96e3adf446070a4a41a2e9fc1a
obijuan <obijuan.cube@gmail.com>
2025-06-12 18:14:52 +0200

    L2: add example test-time

---
+++ wiki/L2/Ejemplos/test-time.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# -- Importar el módulo time
+import time
+
+
+# -- Imprimir primer mensaje
+print()
+print("hola")
+
+# -- Esperar un segundo
+time.sleep(1)
+
+# -- Imprimir el segundo mensaje
+print("hola")
+
+# -- Esperar otro segundo
+time.sleep(1)
+
+
+# -- Imprimir tercer mensaje
+print("hola")
+
+# -- Hacer la última pausa
+time.sleep(1)
+
+# -- Imprimir el cuarto mensaje
+print("hola")
+print()

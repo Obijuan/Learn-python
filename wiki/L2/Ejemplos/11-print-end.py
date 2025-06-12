30e0596fe7f8147faca671b650407bef7e8e7699
obijuan <obijuan.cube@gmail.com>
2025-06-12 12:07:16 +0200

    L2: add example 11

---
+++ wiki/L2/Ejemplos/11-print-end.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+print()
+print("Terminador de línea por defecto:")
+print("Hola")
+print("Mundo")
+
+print()
+print("Impresiones sin terminador")
+print("Hola", end='')
+print("Mundo")
+
+print()
+print("Impresiones con Otro terminador")
+print("Hola", end='*')
+print("Mundo")
+print()

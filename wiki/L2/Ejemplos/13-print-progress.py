a4d923b1fca4d7cc832d48e585422d5302eca383
obijuan <obijuan.cube@gmail.com>
2025-06-13 21:38:05 +0200

    L2: add example 13 with animation

---
+++ wiki/L2/Ejemplos/13-print-progress.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import time
+
+# -- Imprimir un carácter, sin salto de línea
+print('*', end='', flush=True)
+
+# -- Pausa
+time.sleep(1)
+
+# -- Lo mismo: Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+# -- Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+# -- Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+print()

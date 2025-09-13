beb5295ccd41099bc533ddf7617d943c8e38d76e
obijuan <obijuan.cube@gmail.com>
2025-09-13 12:08:36 +0200

    L3: add example

---
+++ wiki/L3/Ejemplos/01-print-progress.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# ──────────────────────────────────────────────────────
+# ── Mini barra de progreso
+# ── Código original
+# ──────────────────────────────────────────────────────
+import time
+
+# ── Imprimir un carácter, sin salto de línea
+print('*', end='', flush=True)
+
+# ── Pausa
+time.sleep(1)
+
+# ── Lo mismo: Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+# ── Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+# ── Impresión + pausa
+print('*', end='', flush=True)
+time.sleep(1)
+
+print()

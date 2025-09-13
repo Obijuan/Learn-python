5cdeb8c4424cc20f408091a451f578b32fd8d848
obijuan <obijuan.cube@gmail.com>
2025-09-13 12:44:00 +0200

    L3: add example

---
+++ wiki/L3/Ejemplos/02-print-progress-func.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# ──────────────────────────────────────────────────────
+# ── Mini barra de progreso
+# ── Código original
+# ──────────────────────────────────────────────────────
+import time
+import ansi
+
+
+# ─────────────────────────────────────────────────────────────────
+# ── Funcion que realiza un paso del progreso actual
+# ── El paso consiste en imprimir un asterisco y esperar un segundo
+# ──────────────────────────────────────────────────────────────────
+def paso():
+    # ── Imprimir un asterisco
+    print(f'{ansi.BLUE}█{ansi.DEFAULT}', end='', flush=True)
+
+    # ── Pausa
+    time.sleep(1)
+
+
+# ─────────────────────────────────────────────────────────────────
+# ── PROGRAMA PRINCIPAL
+# ── MAIN
+# ─────────────────────────────────────────────────────────────────
+# ── Inicializar la pantalla
+print(ansi.CLS, end='', flush=True)
+print(ansi.CURSOR_OFF, end='', flush=True)
+
+# ── Mensaje inicial
+print("Barra de progreso:")
+time.sleep(1)
+
+# ── Dar 4 pasos en el progreso
+paso()
+paso()
+paso()
+paso()
+
+print()
+
+# ── Llevar la pantalla al estado inicial
+print(ansi.CURSOR_ON)
